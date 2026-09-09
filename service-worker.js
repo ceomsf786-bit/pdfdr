2077332b5e144baf78f8b5eb1c973413acdffc80
ceomsf786-bit <ceomsf786@gmail.com>
2026-09-09 07:32:35 +0200

    Bump cache for v3.11 shared library

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,12 @@
+// v3.11 shared library/categories/master backup
 // v3.10 backup/import + teaching flow
 // v3.9 gallery drag/pan/draw fix
 // v3.8 freeform gallery board
 // v3.7 global gallery + image annotations
 // v3.6 realtime annotations + rich notes + student pan
 // v3.5 student-control cache refresh
-const CACHE = 'snt-pdf-static-v3-10';
-const STATIC = ['./','./index.html','./teacher.html','./viewer.css','./viewer.js','./config.js'];
+const CACHE = 'snt-pdf-static-v3-11';
+const STATIC = ['./','./index.html','./teacher.html','./viewer.css','./viewer.js','./config.js','./v310-backup.css','./v310-backup.js','./v311-library.css','./v311-library.js'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(c => c.addAll(STATIC)).catch(()=>{}));
